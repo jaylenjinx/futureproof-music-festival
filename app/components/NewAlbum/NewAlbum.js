564c37101884dde5e5058f038391fca0231ee0a5
Jay <jaylen@sulfurdesign.com>
2025-11-15 07:09:41 +0000

    Remove Old Shows

--- app/components/NewAlbum/NewAlbum.js
+++ app/components/NewAlbum/NewAlbum.js
@@ -2,8 +2,8 @@ export default function NewAlbum({ customClasses }) {
 	return (
 		<div className={`flex flex-col items-center justify-center p-4${customClasses ? ` ${customClasses}` : ""}`}>
 			<h4 className="text-white font-permanentMarker text-xl">Next Event</h4>
-			<h4 className="text-white font-warpDrive text-xl">Generate</h4>
-			<p className="text-fluo-green text-base">Happening 4th of August</p>
+			<h4 className="text-white font-warpDrive text-xl">TBA</h4>
+			<p className="text-fluo-green text-base">TBA</p>
 		</div>
 	);
 }
